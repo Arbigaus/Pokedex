6083a7079acecee95e2f05e45e5cc2bf24eac394
Gerson Arbigaus <gerson87@gmail.com>
2020-06-07 19:15:31 -0300

    structure and configurations

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jasmine: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'FunctionExpression',
+        message: 'Function expressions are not allowed.',
+      },
+      {
+        selector:
+          "CallExpression[callee.name='setTimeout'][arguments.length!=2]",
+        message: 'setTimeout must always be invoked with two arguments.',
+      },
+    ],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+};
