45f3e1f95eff6cca9a68c5094d7a0a2a99aa99c5
Gerson Arbigaus <gerson87@gmail.com>
2020-06-08 22:06:12 -0300

    add poke stats

--- src/screens/Pokemon/components/pokeStats/index.js
+++ src/screens/Pokemon/components/pokeStats/index.js
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-import { Container, Text } from './styles';
+import {
+  Container,
+  Text,
+  StatsTitle,
+  TitleArea,
+  InfoArea,
+  InfoLineArea,
+  StatName,
+  StatValue,
+  StatBar,
+  StatBarBackground,
+  TextInfoArea,
+} from './styles';
 
-const PokeStats = () => {
+import Colors from '../../../../utils/Colors';
+
+const PokeStats = ({ pokeData }) => {
+  const [color, setColor] = useState('white');
+
+  useEffect(() => {
+    if (pokeData.types) {
+      const { types } = pokeData;
+      const typeName = types[0].type.name;
+      setColor(Colors.type[typeName]);
+    }
+  }, [pokeData]);
+
   return (
     <Container>
-      <Text>PokeStats</Text>
+      {pokeData === {} ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <TitleArea>
+            <StatsTitle color={color}>Base Stats</StatsTitle>
+          </TitleArea>
+          <InfoArea>
+            {pokeData.stats &&
+              pokeData.stats.map((stat) => (
+                <InfoLineArea>
+                  <TextInfoArea>
+                    <StatName>{stat.name}</StatName>
+                    <StatValue>{stat.baseStat}</StatValue>
+                  </TextInfoArea>
+                  <StatBar>
+                    <StatBarBackground baseStat={stat.baseStat} color={color} />
+                  </StatBar>
+                </InfoLineArea>
+              ))}
+          </InfoArea>
+        </>
+      )}
     </Container>
   );
+};
+
+PokeStats.propTypes = {
+  pokeData: PropTypes.objectOf(PropTypes.object).isRequired,
 };
 
 export default PokeStats;
--- src/screens/Pokemon/components/pokeStats/styles.js
+++ src/screens/Pokemon/components/pokeStats/styles.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.SafeAreaView`
   flex: 2;
 
-  align-content: center;
-  justify-content: center;
   background-color: #e5e5e5;
   border-top-right-radius: 30px;
   border-top-left-radius: 30px;
@@ -13,4 +11,78 @@ export const Container = styled.SafeAreaView`
 export const Text = styled.Text`
   width: 100%;
   text-align: center;
+`;
+
+export const TitleArea = styled.View`
+  width: 100%;
+  height: 35px;
+  margin-top: 35px;
+  margin-bottom: 35px;
+  padding-left: 40px;
+  padding-right: 40px;
+
+  display: flex;
+  justify-content: center;
+`;
+
+export const StatsTitle = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 19px;
+  color: ${(props) => props.color};
+`;
+
+export const InfoArea = styled.View`
+  width: 100%;
+
+  padding-left: 40px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InfoLineArea = styled.View`
+  height: 35px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+`;
+
+export const TextInfoArea = styled.View`
+  flex: 1;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const StatName = styled.Text`
+  font-size: 12px;
+  line-height: 14px;
+  font-weight: 500;
+  text-transform: capitalize;
+
+  min-width: 110px;
+`;
+
+export const StatValue = styled.Text`
+  font-size: 16px;
+  line-height: 19px;
+  text-align: right;
+  color: #747476;
+`;
+
+export const StatBar = styled.View`
+  flex: 1;
+
+  justify-content: space-around;
+  margin-left: 20px;
+`;
+
+export const StatBarBackground = styled.View`
+  width: ${(props) => `${props.baseStat}%`};
+
+  height: 4px;
+  border-radius: 2px;
+  background-color: ${(props) => props.color};
 `;
